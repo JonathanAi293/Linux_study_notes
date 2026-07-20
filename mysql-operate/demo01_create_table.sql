faf9d74186a9c31a7f6e4269c4d35aefdf48bc15
JonathanAi293 <t8mvrbmdjm@privaterelay.appleid.com>
2026-07-20 10:36:50 +0800

    Create and modify tb_user table with data operations
    Created and modified user table with data insertion and updates.

--- mysql-operate/demo01_create_table.sql
+++ mysql-operate/demo01_create_table.sql
@@ -1,14 +1,16 @@
---创建、修改用户表
---用途：练习MySQL表的创建、字段修改
+--demo01
+--用途：练习MySQL表的创建、字段修改、数据的插入与修改
 
+--建表
 create table tb_user (
 id int comment ' 编号 ',
 name varchar (50) comment ' 姓名 ',
 age int comment ' 年龄 ',
 gender varchar (1) comment ' 性别 '
-) comment ' 用户表 ';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '用户表';
 desc tb_user;
 
+--修改、删除字段
 alter table tb_user add nickname varchar(20) comment '昵称';
 desc tb_user;
 alter table tb_user change nickname username varchar(30) comment '用户名';
@@ -16,5 +18,13 @@ desc tb_user;
 alter table tb_user drop username;
 desc tb_user;
 alter table tb_user rename to user_table;
-desc tb_user;
 desc user_table;
+
+--添加、修改、删除数据
+insert into user_table values(1, '李明', 25, '男');
+insert into user_table values(2, '王琳', 24, '女');
+select * from user_table;
+update user_table set age=26 where id=1;
+select * from user_table;
+delete from user_table where id=2;
+select * from user_table;
